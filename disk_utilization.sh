157e83aa968c2fb67fa82bc5b46cca477346b26d
officialarun <officialarunpandey0600@gmail.com>
2024-11-29 08:48:58 +0530

    Update disk_utilization.sh

--- disk_utilization.sh
+++ disk_utilization.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 THRESHOLD=30
-# Use grep to filter the '/' line and awk to extract the usage percentage
+
 USAGE=$(df -h | grep ' /$' | awk '{print $5}' | awk '{print substr($0, 1, length($0)-1)}') # Removes '%'
 
 if [ "$USAGE" -gt "$THRESHOLD" ]; then
     echo "Disk usage critical: ${USAGE}% (Threshold: ${THRESHOLD}%)"
-    exit 1 # Non-zero exit code for critical disk usage
+    exit 1 
 else
     echo "Disk usage is under control: ${USAGE}% (Threshold: ${THRESHOLD}%)"
-    exit 0 # Zero exit code for normal usage
+    exit 0
 fi
